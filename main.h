cba870a01c0e66d2326835a6374f8ab630059f38
Motlamelle <lebomotlamelle@gmail.com>
2022-05-17 00:05:06 +0200

    add mandatory files

---
+++ main.h
@@ -0,0 +1,8 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+char *get_prompt(void);
+char **split_line(char *str);
+int  exit_shell(void);
+
+#endif
---
+++ prompt.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * get_prompt- read a command from std input
+ *
+ * Return: the command
+ */
+
+char *get_prompt(void)
+{
+	char *buf;
+	size_t size = 64;
+
+	buf = malloc(sizeof(char) * size);
+
+	printf("$ ");
+	getline(&buf, &size, stdin);
+
+	return (buf);
+}
+
+/**
+ * split_line- splits a command line into strings
+ * @str: input line
+ *
+ * Return: Array of strings
+ */
+
+char **split_line(char *str)
+{
+	int i = 0;
+	int bufsize = 64;
+	char *res;
+
+	char **tokens = malloc(bufsize * sizeof(char *));
+
+	/*get first token */
+	tokens[i++] = strtok(str, " ");
+
+	/*get all other tokens */
+	while ((res = strtok(NULL, " ")))
+	{
+		tokens[i++] = res;
+	}
+
+	tokens[i] = NULL;
+
+	return (tokens);
+}
+
+/**
+ * exit_shell - exits the shell
+ *
+ * Return: 0
+ */
+
+int exit_shell(void)
+{
+	return (0);
+}
